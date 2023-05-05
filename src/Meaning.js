4f9d6beeb69d636acd1e85e922b64722693fc81d
laumo15 <118842475+laumo15@users.noreply.github.com>
2023-05-05 22:28:38 +0200

    Added synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
     return (
       <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function (definitions, index) {
+          console.log(definitions);
             return (
               <div key={index}>
-                {definitions.definition} <br /> <em>{definitions.example}</em>
+                {definitions.definition} <br />
+                <em>{definitions.example}</em>
+                <Synonyms synonyms = {definitions.synonyms} />
               </div>
             )
         })}
